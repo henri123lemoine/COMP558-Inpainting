9462bee80084c3ed6066d7387757c171c7e5c036
Henri Lemoine <henri123lemoine@gmail.com>
2024-11-30 03:35:50 -0500

    added convenience init

---
+++ src/algorithms/classical/__init__.py
@@ -0,0 +1,5 @@
+from .efros_leung import EfrosLeungInpainting
+from .navier_stokes import NavierStokesInpainting
+from .patch_match import PatchMatchInpainting
+
+__all__ = ["EfrosLeungInpainting", "NavierStokesInpainting", "PatchMatchInpainting"]
